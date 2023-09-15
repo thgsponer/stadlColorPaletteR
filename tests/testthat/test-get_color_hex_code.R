fafe1bc38a8d17c22db7b27997a6602fa4191978
Thomas Gsponer <thomas.gsponer@stadl-partners.ch>
2023-09-15 17:08:11 +0200

    feat: add function to create colors for interpolation

--- tests/testthat/test-get_color_hex_code.R
+++ tests/testthat/test-get_color_hex_code.R
@@ -11,3 +11,26 @@ test_that("get_color_hex_code works properly and show error if needed", {
   expect_error(get_color_hex_code("mediumorchid1", base_colors = asdf), "object 'asdf' not found")
 })
 
+
+
+  set.seed(123)
+  myc <- tibble(color_names = sample(colors(),5), 
+                     hex_code = colorRampPalette(color_names)(length(color_names)))
+  
+  p2 <- combn(get_color_hex_code(base_colors = myc),2)
+  colnames(p2) <- paste('pal',1:ncol(p2), sep = '')
+  p2 <- as.list(
+    as.data.frame(p2)
+  )
+
+  p1 <- generate_colors_to_interpolate(colors = myc, 2)
+  
+  testthat::expect_true(
+    identical(p1, p2)
+  )
+  
+  testthat::expect_error(generate_colors_to_interpolate(colors = myc, 0), 
+               "n must be greater than 1")
+
+  testthat::expect_error(generate_colors_to_interpolate(colors = myc, 0))
+  
